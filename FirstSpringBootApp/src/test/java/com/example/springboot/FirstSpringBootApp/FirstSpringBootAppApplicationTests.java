7b5298517c2409746e52f99d570c60942eefd743
nehachaudh <it.neha@gmail.com>
2023-05-20 22:28:04 +0530

    first commit

---
+++ FirstSpringBootApp/src/test/java/com/example/springboot/FirstSpringBootApp/FirstSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.FirstSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
